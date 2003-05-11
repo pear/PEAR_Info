626aa023b8b43853090ebcff84537aeee923ff21
Davey Shafik <davey@php.net>
2003-05-11 10:58:49 +0000

    'Fixed typo'
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_Info/trunk@126546 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/pear_info.php
+++ tests/pear_info.php
@@ -24,7 +24,7 @@ ini_set('max_execution_time',600);
 ini_set('max_input_time',600); */
 
 // If you need to set a http_proxy uncomment the line below
-// PEAR_Info::setProxy('you.proxy.here');
+// PEAR_Info::setProxy('your.proxy.here');
 
 // require the PEAR_Info file
 require_once 'PEAR/Info.php';
