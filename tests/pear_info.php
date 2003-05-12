908b742ec8bc3d2693d6368e6e54e5bc9d06a699
Davey Shafik <davey@php.net>
2003-05-12 11:40:57 +0000

    'moved PEAR_Info::setProxy() call to below the require_once for the class.'
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_Info/trunk@126641 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/pear_info.php
+++ tests/pear_info.php
@@ -23,11 +23,11 @@ ini_set('default_socket_timeout',600);
 ini_set('max_execution_time',600);
 ini_set('max_input_time',600); */
 
-// If you need to set a http_proxy uncomment the line below
-// PEAR_Info::setProxy('your.proxy.here');
-
 // require the PEAR_Info file
 require_once 'PEAR/Info.php';
+
+// If you need to set a http_proxy uncomment the line below
+// PEAR_Info::setProxy('your.proxy.here');
 
 // Create PEAR_Info object
 $info = new PEAR_Info();
