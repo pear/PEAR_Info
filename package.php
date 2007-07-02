d3423241d12805def67114fbb7270d475d87b5a1
Laurent Laville <farell@php.net>
2007-07-02 20:39:01 +0000

    prepare 1.7.0RC2 that fixed a problem with pear config files, and ability to display all channels information wihtout giving their names
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_Info/trunk@239040 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -39,40 +39,24 @@ $p2 = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
 $p2->setPackageType('php');
 $p2->generateContents();
 $p2->addRelease();
-$p2->setReleaseVersion('1.7.0RC1');
+$p2->setReleaseVersion('1.7.0RC2');
 $p2->setAPIVersion('1.7.0');
 $p2->setReleaseStability('beta');
 $p2->setAPIStability('stable');
-$p2->setNotes('* news
-- Full compatibility with PEAR 1.3.x, PEAR 1.4.x, PEAR 1.5.x
-- Easy change look and feel with a simple CSS file
-- Show packages dependencies list
-- HTML page output is XHTML compliant
-- Introduce a full and flexible configuration system to show only information you want
-  (see example pear_info5.php)
-
-Only for PEAR 1.4.x or better :
-- Show channel list
-- Show maintainer inactive status in Credits page
-- Support REST 1.0 protocol
-
-* changes
-- drop support of package xml 1.0
-  (PEAR 1.3.x are encourage to upgrade to PEAR 1.4.x or better)
-- Minimum PEAR required is now 1.4.11
-- Minimum PHP version is now set to 4.3.0 (due to usage of file_get_contents function)
+$p2->setNotes('* changes
+- "channels" key option not defined give only the pear.php.net channel information
+- "channels" key option with a list give only this list of channels
+- "channels" key option with an empty array give all channels information
+  (see new example pear_info6.php)
 
 * bugs
-- fixed bug #6050 - PEAR version displayed as Array instead of version number
-- fixed bug #8165 - Error on code
-- fixed req #7741 - new feature and pear 1.4.x full compatible
-- fixed req #10815 - Quick list of packages
+- fixed bug #11489 - Configuration file warning
 ');
 //$p2->setLicense('PHP License 3.01', 'http://www.php.net/license/3_01.txt');
 //$p2->addMaintainer('lead', 'farell', 'Laurent Laville', 'pear@laurent-laville.org');
-$p2->addReplacement('Info.php', 'pear-config', '@data_dir@', 'data_dir');
-$p2->addReplacement('Info.php', 'package-info', '@package_name@', 'name');
-$p2->addReplacement('Info.php', 'package-info', '@package_version@', 'version');
+//$p2->addReplacement('Info.php', 'pear-config', '@data_dir@', 'data_dir');
+//$p2->addReplacement('Info.php', 'package-info', '@package_name@', 'name');
+//$p2->addReplacement('Info.php', 'package-info', '@package_version@', 'version');
 
 if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
     $p2->writePackageFile();
